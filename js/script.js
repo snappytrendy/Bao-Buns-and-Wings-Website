11c5229d1d1793cecb235bd38e560d4259d729a1
Diana Rose Munuhe <dianamunuhe24@gmail.com>
2026-07-09 16:51:45 +0300

    started on script

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-// ==========================================
-// HERO ENTRANCE ANIMATION
-// ==========================================
-
-document.addEventListener("DOMContentLoaded", () => {
-  const heroContent = document.querySelector(".hero-content");
-  const heroImage = document.querySelector(".hero-image");
-
-  heroContent.classList.add("show");
-
-  setTimeout(() => {
-    heroImage.classList.add("show");
-  }, 300);
+window.addEventListener("load", () => {
+  document.querySelector(".hero-content").classList.add("show");
+  document.querySelector(".hero-image").classList.add("show");
 });
+// ==========================================
+// SCROLL REVEAL
+// ==========================================
+
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  reveals.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < triggerBottom) {
+      section.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
